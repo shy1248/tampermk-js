56f6b7a835ff80084987dd652158d1ebd0cb2a44
yushuibo <hengchen2005@gmail.com>
2021-05-03 19:50:24 +0800

    add function for beautyfont.js

--- beautyfont.js
+++ beautyfont.js
@@ -15,13 +15,71 @@
 // @run-at       document-start
 // @grant        unsafeWindow
 // @license      MIT
-// @require      https://greasyfork.org/scripts/29500-%E5%BC%BA%E5%88%B6%E4%BD%BF%E7%94%A8%E5%AD%97%E4%BD%93-%E6%A0%B8%E5%BF%83/code/%E5%BC%BA%E5%88%B6%E4%BD%BF%E7%94%A8%E5%AD%97%E4%BD%93%20-%20%E6%A0%B8%E5%BF%83.js
 // ==/UserScript==
 
 // ===============
 // 遇到显示方框请手动添加排除或反馈
 // ===============
+
 (function () {
     // change the font family for yourself favorite.
-    changeFont("PingFang SC" ,1);
+    changeFont("PingFang SC", 0);
+
+    function changeFont(s_font, mode) {
+        if (s_font !== "") {
+
+            switch (mode) {
+                case 0:
+                    var element = document.createElement("link");
+                    element.rel = "stylesheet";
+                    element.type = "text/css";
+                    element.href = 'data:text/css,*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+                    document.documentElement.appendChild(element);
+                    break;
+                case 1:
+                    setTimeout(function () {
+                        var modStyle = document.querySelector('#modCSS_font');
+                        if (modStyle === null) {
+                            modStyle = document.createElement('style');
+                            modStyle.id = 'modCSS_font';
+                            document.body.appendChild(modStyle);
+                        }
+                        modStyle.innerHTML = '*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+                    }, 300);
+                    break;
+                case 2:
+                    var element = document.createElement("link");
+                    element.rel = "stylesheet";
+                    element.type = "text/css";
+                    element.href = 'data:text/css,*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="hwic"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont,sans-serif !important;}';
+                    document.documentElement.appendChild(element);
+                    setTimeout(function () {
+                        var modStyle = document.querySelector('#modCSS_font');
+                        if (modStyle === null) {
+                            modStyle = document.createElement('style');
+                            modStyle.id = 'modCSS_font';
+                            document.body.appendChild(modStyle);
+                        }
+                        modStyle.innerHTML = '*:not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]):not(i){font-family:' + s_font + ',Arial,"Material Icons Extended",stonefont,iknow-qb_share_icons,review-iconfont,mui-act-font,fontAwesome,tm-detail-font,office365icons,MWF-MDL2,global-iconfont,"Bowtie",myfont !important;}';
+                    }, 300);
+                    break;
+                case 3:
+                    var element = document.createElement("link");
+                    element.rel = "stylesheet";
+                    element.type = "text/css";
+                    element.href = 'data:text/css,*:not(i):not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]){font-family:' + s_font + ',Arial,"Material Icons Extended";}';
+                    document.documentElement.appendChild(element);
+                    setTimeout(function () {
+                        var modStyle = document.querySelector('#modCSS_font');
+                        if (modStyle === null) {
+                            modStyle = document.createElement('style');
+                            modStyle.id = 'modCSS_font';
+                            document.body.appendChild(modStyle);
+                        }
+                        modStyle.innerHTML = '*:not(i):not([class*="icon"]):not([class*="fa"]):not([class*="logo"]):not([class*="mi"]):not([class*="code"]){font-family:' + s_font + ',"Material Icons Extended",Arial,"Material Icons Extended";}';
+                    }, 300);
+                    break;
+            }
+        }
+    }
 })();
